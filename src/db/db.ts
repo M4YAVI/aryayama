4156f9b8ec2c5c1af0cb60d4f382544fa22a8983
sangeeth-Chejerla <sangeethreddychejerla@gmail.com>
2025-06-11 14:20:45 +0530

    finding error

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,9 +1,9 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
+import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/libsql';
 
 const turso = createClient({
   url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
 export const db = drizzle(turso);
